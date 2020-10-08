da5d6e16d9876622b83177b5d0d441f1b3f660f2
Jamie Phillips <phillipsja97@gmail.com>
2020-10-07 19:44:47 -0500

    screen designed

--- src/Styles/Styles.js
+++ src/Styles/Styles.js
@@ -18,27 +18,4 @@ export const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  logo: {
-    height: '100%',
-    width: 500,
-    marginBottom: 10,
-    backgroundColor: 'transparent'
-  },
-  splashTitleContainer: {
-    position: 'absolute',
-    justifyContent: 'center',
-    left: 0,
-    right: 0,
-    top: 0,
-    bottom: 0,
-    alignItems: 'center',
-    color: 'white'
-  },
-  splashTitleText: {
-    color: 'white',
-    fontSize: 26,
-  },
-  splashButtons: {
-    margin: 10
-  }
 });
